b408a678f3a6916a3b28a9e9f947faa3412719b8
Smurf <sudaru2@gmail.com>
2019-10-11 21:41:41 +0300

    optimise graph init;add amountOfVertex and amountOfEdges fiels and getters for them

--- graph.go
+++ graph.go
@@ -2,18 +2,32 @@ package graphlib
 
 //Graph is struct that describe graph with vertex edges popose in map
 type Graph struct {
-	graph      [][]int
+	graph          [][]int
+	amountOfVertex int
+	amountOfEdges  int
 }
 
 //Init is initialize graph with nonnil map
-func (g *Graph)Init(){
-	g.graph = make([][]int,0,0)
+func (g *Graph) Init(v, e int) {
+	g.amountOfVertex = v
+	g.amountOfEdges = e
+	g.graph = make([][]int, v)
+}
+
+//AmountOfVertex returns amount of graph edges
+func (g *Graph) AmountOfVertex() int {
+	return g.amountOfVertex
+}
+
+//AmountOfEdges returns amount of edges in graph
+func (g *Graph) AmountOfEdges() int {
+	return g.amountOfEdges
 }
 
 //AddVertex function is adding new vertex without edges t graph
 func (g *Graph) AddVertex() {
 
-	g.graph = append(g.graph,make([]int,1))
+	g.graph = append(g.graph, make([]int, 1))
 
 }
 
@@ -25,7 +39,7 @@ func (g *Graph) AddEdgesToVertex(vertex int, edges []int) {
 
 //AddVertexWithEdges function is adding new vertex with edges contained in @edges
 func (g *Graph) AddVertexWithEdges(edges []int) {
-	g.graph = append(g.graph,edges)
+	g.graph = append(g.graph, edges)
 
 }
 
@@ -36,5 +50,5 @@ func (g *Graph) GetEdges(ver int) []int {
 
 //Size return amount of added vertex
 func (g *Graph) Size() int {
-	return len(g.graph)-1
+	return len(g.graph) - 1
 }
